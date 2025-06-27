9e911f21c096524f708434abd88038ced7cd2963
nicole <nicole.mos.mosquera@gmail.com>
2025-06-26 19:55:20 -0500

    script: data types in python

---
+++ example1.py
@@ -0,0 +1,24 @@
+# this is an example whit Python
+
+'''
+    Date: 06-jun-2025
+    Dav: Marlon Lopes
+'''
+data1 = 10
+data2 = 3,50
+data3 = "MArlon L."
+data4 = 'Lopes'
+data5 = '''
+            Hello people, we are learning Python.
+            That's wonderful.
+        '''
+data6 = True
+data7 = 3.4
+
+print(type(data1))
+print(type(data2))
+print(type(data3))
+print(type(data4))
+print(type(data5))
+print(type(data6))
+print(type(data7))
